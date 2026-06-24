80b9b2112d287133a7a75fae2e9ec14f2e52bc08
Khushraj Rathod <khushraj.rathod@gmail.com>
2026-06-24 20:08:30 +0530

    Make trust script fallback to local user store

--- trust.ps1
+++ trust.ps1
@@ -1,4 +1,12 @@
 $url = "https://files.khushrajrathod.com/ca.crt"
 $path = "$env:TEMP\ca.crt"
-Start-Process powershell -Verb RunAs -WindowStyle Hidden -Wait -ArgumentList "-NoProfile -Command `"Invoke-WebRequest -Uri '$url' -OutFile '$path'; Import-Certificate -FilePath '$path' -CertStoreLocation Cert:\LocalMachine\Root`""
+
+Invoke-WebRequest -Uri $url -OutFile $path
+
+try {
+    Start-Process powershell -Verb RunAs -WindowStyle Hidden -Wait -ErrorAction Stop -ArgumentList "-NoProfile -Command `"Import-Certificate -FilePath '$path' -CertStoreLocation Cert:\LocalMachine\Root`""
+} catch {
+    Import-Certificate -FilePath $path -CertStoreLocation Cert:\CurrentUser\Root
+}
+
 Read-Host "Certificate install completed successfully, press enter to exit"
