88d22a7faa756f1ebe1b807460de68cb71ca67c0
Khushraj Rathod <khushraj.rathod@gmail.com>
2026-06-24 19:52:38 +0530

    Amend CA script

--- trust.ps1
+++ trust.ps1
@@ -1,4 +1,4 @@
-$url = "http://files.khushrajrathod.com/ca.crt"
+$url = "https://files.khushrajrathod.com/ca.crt"
 $path = "$env:TEMP\ca.crt"
 Start-Process powershell -Verb RunAs -WindowStyle Hidden -Wait -ArgumentList "-NoProfile -Command `"Invoke-WebRequest -Uri '$url' -OutFile '$path'; Import-Certificate -FilePath '$path' -CertStoreLocation Cert:\LocalMachine\Root`""
 Read-Host "Certificate install completed successfully, press enter to exit"
